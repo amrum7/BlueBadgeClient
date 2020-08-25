21f321cca0e24b8383312586b513bb0b5b560c58
Adam Rumschlag <adamrumschlag@Adams-MacBook-Pro.local>
2020-08-25 19:37:45 -0400

    first

--- src/auth/Signup.js
+++ src/auth/Signup.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { Form, FormGroup, Label, Input, Button } from "reactstrap";
+import APIURL from "../helpers/enviornment";
 
 const Signup = (props) => {
   const [username, setUsername] = useState("");
